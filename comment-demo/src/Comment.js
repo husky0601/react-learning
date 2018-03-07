7ad132feeeb462f2035394c2b4b916634760a6fe
husky0601 <suyating228@gmail.com>
2018-03-07 09:21:10 +0800

    整合

--- comment-demo/src/Comment.js
+++ comment-demo/src/Comment.js
@@ -1,13 +1,45 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
 class Comment extends Component {
+    // 在组件comment类组件中添加propType类属性
+    static propTypes = {
+        comment: PropTypes.object.isRequired
+    }
+
+    constructor() {
+        super()
+        this.state = {
+            timeString: ''
+        }
+    }
+
+    componentWillMount(){
+        this._updateTimeString()
+        this._timer = setInterval(
+            this._updateTimeString.bind(this),
+            10000
+        )
+    }
+
+    // 更新发布的时间差
+    _updateTimeString(){
+        const comment = this.props.comment
+        const duration = (+Date.now() - comment.createdTime) / 1000
+        this.setState({
+            timeString: duration > 60 ? `${Math.round(duration / 60)} 分钟前`
+            :  `${Math.round(Math.max(duration, 1))} 秒前`
+        })
+    }
     render() {
+        const { comment } =this.props
         return (
             <div className='comment'>
-                <div className='comment-user'>
-                    <span>{this.props.comment.username}: </span>
+                <div className='comment-username'>
+                    <span>{comment.username}: </span>
                 </div>
-                <p> {this.props.comment.content}</p>
+                <p> {comment.content}</p>
+                <span className='comment-createdtime'>{this.state.timeString}</span>
             </div>
         )
     }
--- comment-demo/src/CommentApp.js
+++ comment-demo/src/CommentApp.js
@@ -10,16 +10,34 @@ class CommentApp extends Component{
             comments: []
         }
     }
+    //DOM渲染前加载评论内容
+    componentWillMount(){
+        this._loadComment()
+    }
 
+    //保存评论至浏览器中
+    _loadComment(){
+        let comments = localStorage.getItem('comments')
+        if(comments){
+            this.setState({ 
+                comments: JSON.parse(comments)
+            })
+        }
+        console.log(this.state.comments)
+    }
+    _saveComment(comments){
+        localStorage.setItem('comments',JSON.stringify(comments))
+    }
+    
     // 获取评论内容
     handleSubmitComment(comment) {
         if(!comment) return
         if(!comment.username) return alert('请输入用户名')
         if(!comment.content) return alert('请输入评论内容')
-        this.state.comments.push(comment)
-        this.setState({
-            comments: this.state.comments
-        })
+        const comments = this.state.comments
+        comments.push(comment)
+        this.setState({ comments })
+        this._saveComment(comments)
     }
 
     render() {
